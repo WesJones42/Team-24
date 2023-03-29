42e0af04e29dd6d3be19613e066df7de6650e99f
mitharbs <mitharbs@gmail.com>
2023-03-29 17:27:47 -0600

    deleting excess

--- local_html/m1/index.php
+++ local_html/m1/index.php
@@ -225,33 +225,4 @@ if (strpos($response->body(), '{exec_time}') !== false or strpos($response->body
  */
 
 $response->send(true);
-=======
-<!DOCTYPE html>
-<html lang="en">
-<meta charset="UTF-8">
-<title>Page Title</title>
-<meta name="viewport" content="width=device-width,initial-scale=1">
-<link rel="stylesheet" href="">
-<style>
-</style>
-<script src=""></script>
-<body color: "green">
-
-<img src="Logo.jpg" alt="LA" style="width:40%">
-<nav>
-  <ul>
-    <li>About</li>
-    <li>Color Gen</li>
-  </ul>
-</nav>
-
-<div class="">
- <h1>Team 24</h1>
- <p>Hi! Welcome to Team 24!</p>
- <p>We do things differently around here...</p>
-</div>
-
-</body>
-</html>
-
 

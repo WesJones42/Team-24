57829ea589bde69ff597d94e63514f49a44adc3f
mitharbs <mitharbs@gmail.com>
2023-03-29 18:16:17 -0600

    Start of About

--- fuel/app/views/view/About.php
+++ fuel/app/views/view/About.php
@@ -1,1 +1,25 @@
+<!DOCTYPE html>
+<html>
+<title>About the Team</title>
+<header>
+<p>About the team behind 24</p>
+</header>
+<body>
+    <div id="Wes"></div>
 
+    
+    <div id="Mitch">
+        <p>Hi, I'm Mitch Harbison. I am a returning second bachelors student here at CSU. My first degree is in BioChemisty and am looking forward to completing a 
+            General Concentration in Computer Science. My goal is to gain the knowledge to help me better understand the human genome.
+        </p>
+    </div>
+
+</body>
+<footer>
+
+</footer>
+
+
+
+
+</html>

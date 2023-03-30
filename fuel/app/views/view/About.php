52dcd626e1d33dee5b8e92f7a9a92835ae0aeb80
mitharbs <mitharbs@gmail.com>
2023-03-29 18:52:49 -0600

    Profile pics

--- fuel/app/views/view/About.php
+++ fuel/app/views/view/About.php
@@ -9,13 +9,16 @@
         <p>
         My name is Wes Jones. I am a senior CS student. I enjoy weightlifting, reading, and playing bass guitar!
         </p>
+        <img  src = "Wes.jpg" alt= "Wes Bio Pic">
     </div>
+
 
     
     <div id="Mitch">
         <p>
         Hi, Im Mitch Harbison. I am a returning second bachelors student here at CSU. My first degree is in BioChemisty and am looking forward to completing a General Concentration in Computer Science. My goal is to gain the knowledge to help me better understand the human genome.
         </p>
+        <img src = "Mitch.jpg" alt= "Mitch Bio Pic">
     </div>
 
 </body>

859e95c55855b44669967db485a378d265ee1a1a
WesJones42 <wesjones11@comcast.net>
2023-03-29 18:29:38 -0600

    added my Bib

--- fuel/app/views/view/About.php
+++ fuel/app/views/view/About.php
@@ -5,12 +5,16 @@
 <p>About the team behind 24</p>
 </header>
 <body>
-    <div id="Wes"></div>
+    <div id="Wes">
+        <p>
+        My name is Wes Jones. I am a senior CS student. I enjoy weightlifting, reading, and playing bass guitar!
+        </p>
+    </div>
 
     
     <div id="Mitch">
-        <p>Hi, I'm Mitch Harbison. I am a returning second bachelors student here at CSU. My first degree is in BioChemisty and am looking forward to completing a 
-            General Concentration in Computer Science. My goal is to gain the knowledge to help me better understand the human genome.
+        <p>
+        Hi, Im Mitch Harbison. I am a returning second bachelors student here at CSU. My first degree is in BioChemisty and am looking forward to completing a General Concentration in Computer Science. My goal is to gain the knowledge to help me better understand the human genome.
         </p>
     </div>
 

ab5f419d3f0890c7d63f72624f3c119a5169e9a4
WesJones42 <wesjones11@comcast.net>
2023-03-30 15:01:29 -0600

    fixed about img size II

--- fuel/app/views/view/About.php
+++ fuel/app/views/view/About.php
@@ -2,14 +2,14 @@
 <html>
 <title>About the Team</title>
 <header>
-<p>About the team behind 24</p>
+<p>About the Freaky Four</p>
 </header>
 <body>
     <div id="Wes">
         <p>
-        My name is Wes Jones. I am a senior CS student. I enjoy weightlifting, reading, and playing bass guitar!
+        My name is Wes Jones. I am a senior CS student. Im doing a concentration in AI/Ml with a minor in math. I hope to be training models in the future. I am particulary interested in AI math applications. I enjoy weightlifting, reading, and playing bass guitar!
         </p>
-        <?php echo Asset::img("Wes.jpg"); ?>
+        <td><p> <?php echo Asset::img("Wes.jpg");?></p> </td>
     </div>
 
 
@@ -18,7 +18,7 @@
         <p>
         Hi, Im Mitch Harbison. I am a returning second bachelors student here at CSU. My first degree is in BioChemisty and am looking forward to completing a General Concentration in Computer Science. My goal is to gain the knowledge to help me better understand the human genome.
         </p>
-        <?php echo Asset::img("Mitch.jpg"); ?>
+        <td><p> <?php echo Asset::img("Mitch.jpg");?></p> </td>
     </div>
 
 </body>

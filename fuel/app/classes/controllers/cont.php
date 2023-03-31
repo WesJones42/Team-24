fc3176423cc5d00fe46b50cf1f03ef72d3009052
WesJones42 <wesjones11@comcast.net>
2023-03-31 15:07:02 -0600

    Controller got controled

--- fuel/app/classes/controllers/cont.php
+++ fuel/app/classes/controllers/cont.php
@@ -1,52 +1,37 @@
 <?php
-class Controller_Cont extends Controller_Template
-{
+class Controller_Cont extends Controller_Template{
 
-    public $template = 'T24Template';
+public $template = 'T24Template';
 
 
-    public function action_index()
+public function action_Home()
     {
-
+        
         $data = array();
         $this->template->title = 'Home Page';
         $this->template->header = "Freaky Four";
         $this->template->content = View::forge('T24/Home.php', $data);
         $this->template->css = "Home.css";
     }
-
-    public function action_About()
+    
+public function action_About()
     {
-
+        
         $data = array();
         $this->template->title = 'About Page';
         $this->template->header = "Freaky Four";
         $this->template->content = View::forge('T24/About.php', $data);
         $this->template->css = "Home.css";
     }
-
-    public function action_colorCoordinates()
+    
+    public function action_colorcoordinates()
     {
         $data = array();
         $this->template->title = 'Color Coordinate Generation';
         $this->template->header = "Freaky Four";
-        $this->template->content = View::forge('view/colorcoordinates', $data);
+        $this->template->content = View::forge('T24/colorcoordinates.php', $data);
         $this->template->css = "Home.css";
     }
 
-    public function get_generate($rows = 0, $colors = 0)
-    {
-        $rows = Input::get('rows');
-        $colors = Input::get('colors');
-
-        if ($rows < 1 || $rows > 26 || $colors < 1 || $colors > 10) {
-            Session::set_flash('error', 'Invalid input. Rows/columns must be between 1 and 26, and colors must be between 1 and 10.');
-            Response::redirect('index/cont/colorcoordinates');
-        }
-
-        $this->template->title = 'Color Coordinate Genearation';
-        $this->template->content = View::forge('view/generate', ['rows' => $rows, 'colors' => $colors]);
-        $this->template->css = "Home.css";
-        $this->template->header = "Freaky Four";
-    }
+    
 }

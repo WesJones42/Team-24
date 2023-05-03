9dcbc06cd85295f7798c287d1a35fd09b05f2951
Wes <97007205+WesJones42@users.noreply.github.com>
2023-05-02 23:13:25 -0600

    Update colorcoordinates.php

--- fuel/app/views/view/colorcoordinates.php
+++ fuel/app/views/view/colorcoordinates.php
@@ -17,6 +17,7 @@
         } else {
             Cur_Colors[x] = Element.value;
             updateCellColors(oldColor, Element.value);
+            Element.style.backgroundColor = Element.value;
     }
 }
 
@@ -34,6 +35,27 @@
 
         function cellClick(cell, col, row) {
           let colorValue = document.querySelector(".selectedColor").value;
+          let currentColor = cell.style.backgroundColor;
+          
+              if (currentColor) {
+                for (let i = 0; i < Cur_Colors.length; i++) {
+                if (Cur_Colors[i] === currentColor) {
+                  let colorTableCell = document.getElementById("table1Cell " + i);
+                  let cellLabels = colorTableCell.textContent.trim().split(", ");
+                  let cellLabelToRemove = col + row;
+
+                  // Find and remove the cell's label from the list
+                  let index = cellLabels.indexOf(cellLabelToRemove);
+                  if (index > -1) {
+                    cellLabels.splice(index, 1);
+                    colorTableCell.textContent = cellLabels.join(", ");
+                  }
+                  break;
+              }
+          }
+      }
+          
+          
           cell.style.backgroundColor = colorValue;
 
           let cellLabel = col + row;
@@ -65,10 +87,12 @@
             });
         }
         
+        
+        
     </script>
 </head>
 <body>
-    <!-- Setup for text boxes and submit button -->
+
     <form method="get" action="colorcoordinates.php">
         <label for="number">Number of Rows/Cols (Max:26)</label>
         <div class="error">
@@ -123,11 +147,11 @@ if ($boolColor == TRUE && $boolNum == TRUE) {
         echo "<option id=\"blank\"></option>";
 
         foreach ($color_array as $colorVals) {
-            echo "<option style=\"background-color:white\" id=\"$colorVals\">$colorVals</option>";
+            echo "<option style=\"background-color:$colorVals\" id=\"$colorVals\">$colorVals</option>";
         }
 
         echo "</select>";
-        echo "<input type=\"radio\" name=\"colorSelection\" onclick=\"selectColor($x)\"" . ($x == 0 ? " checked" : "") . ">";
+        echo "<input type=\"radio\" id=\"radio$x\" name=\"colorSelection\" onclick=\"selectColor($x)\"" . ($x == 0 ? " checked" : "") . ">";
         echo "</td>";
         echo "<td id=\"table1Cell $x\" style=\"width:80%\"></td>";
         echo "</tr>";
@@ -169,10 +193,71 @@ if ($boolColor == TRUE && $boolNum == TRUE) {
     echo "</div>";
 }
 
-echo "<button id=\"printButton\">Print (Doesn't Work)</button>"; //Wish we had a 4th to do this =(
+echo "<button id=\"printButton\">Print</button>"; //Wish we had a 4th to do this =(
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
 ?>
 
 
 </script>
-</html>
+<script>
+$("#printButton").click(function(){
+        
+        $("#table1").toggleClass("grayScale");
+        $("#table2").toggleClass("grayScale");
+        console.log("Print button was clicked")
+        openWin();
+    });
+    function openWin() {
 
+    var num = <?php echo $color;?>;
+    console.log(num);
+    
+    var newTable1 = document.createElement("table");
+    newTable1.border = "1px";
+    newTable1.style.width = "100%";
+    
+    for (var i = 0; i < num; i++) {
+      var row = newTable1.insertRow(-1);
+      var colorCell = row.insertCell(0);
+      var coordCell = row.insertCell(1);
+
+      var selectedColor = document.getElementById("dropMenu " + i).value;
+      colorCell.innerHTML = selectedColor; // Set text to be the color name
+
+      var coordsText = document.getElementById("table1Cell " + i).textContent;
+      coordCell.innerHTML = coordsText;
+    }
+
+    //Remove radio buttons
+    for(var i = 0; i < num; i ++){
+        var elem = document.getElementById("radio" + i);
+        console.log("Elem test: ", elem);
+    
+            
+            elem.parentNode.removeChild(elem);
+            console.log("Removed Child");
+        
+    }
+
+    var myWindow = window.open();
+
+    // Check if the window was successfully opened
+    if (myWindow) {
+        var table2 = $("#table2").html();
+        var t2 = document.querySelector("#table2");
+
+        myWindow.document.write(newTable1.outerHTML);
+        myWindow.document.write(table2);
+        
+        myWindow.print();
+    } else {
+        // Show an alert if the window is null (blocked by popup blocker)
+        alert("Man, Remove your damn popup blocker!");
+    }
+}
+</script>
+</html>
